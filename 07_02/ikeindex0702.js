5e1f6e9f9105bdbc8e0efa3fcfe3dc5afff9ea40
Owt And About <70513244+Ikemefula@users.noreply.github.com>
2021-05-30 13:59:37 -0500

    javascript objects

---
+++ 07_02/ikeindex0702.js
@@ -0,0 +1,14 @@
+// ARRAY
+
+
+// Javascript MEthods
+    // Pop
+    // Push MEthods 
+    // Shift 
+
+// .length property -
+
+let item = "flashlight";
+const collection = ["Piggy", item, 5, true];
+
+console.log(collection);
