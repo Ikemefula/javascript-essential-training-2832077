7338f6625ce6dbd89897df56149327c29a04bb2e
Owt And About <70513244+Ikemefula@users.noreply.github.com>
2021-05-30 00:04:09 -0500

    update

--- 06_08/ikescript.js
+++ 06_08/ikescript.js
@@ -6,9 +6,10 @@ console.log('let a: ${a} (${typeof a})');
 console.log('let a: ${b} (${typeof b})');
 console.log('let a: ${c} (${typeof c})');
 
+
 // let result = a + b;
 // let result = a ** b;
-let result = a ** b;
+// let result = a ** b;
 
 
 // Pre increment
@@ -33,3 +34,47 @@ let result = a ** b;
 
 // Modulus/Modulo - Returns the remainder of two divided variable. 
 // Used to test for PRIME value 
+
+
+// Assignment Operators 
+// =
+    // x = y is equal to x = y
+// += 
+    // x += y is equal to x = x+y
+// -= 
+    // x-= y is equal to x = x - y
+// *= 
+    // x *= y  is equal to x = x * y
+// /= 
+    // x /= y is equal to x = x / y
+// %= 
+// x %= y is equal to x = x % y
+
+
+
+
+// Comparison Operators 
+// ==
+    // Equal to
+        // 5 == 10 false
+
+// ===
+    // identical - equal and of same type
+// !=
+    // not Equal to
+
+// !== 
+    // not identical
+
+// >  
+    // Greater than
+
+// >=
+    // Greater than or equal to
+
+// <
+    // Less than
+
+// <= 
+    // Less than or equal
+
