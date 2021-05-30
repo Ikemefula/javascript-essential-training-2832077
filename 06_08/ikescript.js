e1a568ace6620f6a8c33c56a23f9fbd1899f5f2e
Owt And About <70513244+Ikemefula@users.noreply.github.com>
2021-05-29 22:33:03 -0500

    fork over
    I've forked this repo so that i can work out my own kinks while following along with the istructor

---
+++ 06_08/ikescript.js
@@ -0,0 +1,30 @@
+let a = 15;
+let b = 4;
+let c = 3.2;
+
+console.log('let a: ${a} (${typeof a})');
+console.log('let a: ${b} (${typeof b})');
+console.log('let a: ${c} (${typeof c})');
+
+// let result = a + b;
+// let result = a ** b;
+let result = a ** b;
+
+
+// console.log("Your Result Are: ", ++a);
+// console.log(a);
+
+// console.log("Your Result Are: ", a++);
+// console.log(a);
+
+// console.log("Your Result Are: ", --a);
+// console.log(a);
+
+// console.log("Your Result Are: ", a--);
+// console.log(a);
+
+
+
+
+// Modulus/Modulo - Returns the remainder of two divided variable. 
+// Used to test for PRIME value 
